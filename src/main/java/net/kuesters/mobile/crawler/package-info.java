d7dd24174d761d83dcbb0c4b27fffb5ae4e31dd1
Jens Kuesters <derteaser@gmail.com>
2014-05-30 13:06:33 +0200

    removed obsolete package info

--- src/main/java/net/kuesters/mobile/crawler/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package contains some utility class that handle the crawling of the mobile app's content.
- * @author <a href="http://www.kuesters.net">Jens K&uuml;sters</a>
- *
- */
-package net.kuesters.mobile.crawler;
